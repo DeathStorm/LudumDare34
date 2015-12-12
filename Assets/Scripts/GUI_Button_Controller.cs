10d7c06eaac032067b50e3f4abb5312e9f0d4635
DeathStorm <Death_Storm@web.de>
2015-12-12 21:49:50 +0100

    Schluck dies

--- Assets/Scripts/GUI_Button_Controller.cs
+++ Assets/Scripts/GUI_Button_Controller.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 using BUILDINGS = Buildings.BUILDINGS;
-using RESSOURCES = Player.RESSOURCES;
+using ASSIGNMENTS = Round_Manager.ASSIGNMENTS;
 
 
 public class GUI_Button_Controller: MonoBehaviour {
 
     public Buildings buildings;
     public BUILDINGS buildingType = BUILDINGS.NONE;
-    public int changeAssignedBuildingAmount = 0;
+
+    public ASSIGNMENTS changeAssigned = ASSIGNMENTS.NONE;
+    public int changeAssignedAmount = 0;
 
 
     public void Click_ChooseBuildingToBuild()
