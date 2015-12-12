592ba89e0a12b68b71e296e44bba893394882701
DeathStorm <Death_Storm@web.de>
2015-12-12 13:08:10 +0100

    stuff

---
+++ Assets/Tile.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour
+{
+    public Sprite tmpImage;
+
+    private SpriteRenderer spriteRenderer;
+    private Sprite standardSprite;
+
+    // Use this for initialization
+    void Start()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+        standardSprite = spriteRenderer.sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseEnter()
+    {
+        spriteRenderer.sprite = tmpImage;
+    }
+
+    void OnMouseExit()
+    {
+        spriteRenderer.sprite = standardSprite;
+    }
+
+
+    void OnMouseDown()
+    {
+        Debug.Log("Fick dich geh weg.....");
+    }
+
+}
